e365c62e980f7a265c95148f15c458f2b04f2d96
DrDuckson <gorealex95@web.de>
2024-05-27 22:29:19 +0200

    Script cleanup

--- Player.gd
+++ Player.gd
@@ -4,13 +4,8 @@ var forceX = 800
 var forceY = 800
 var zero = 0
 
-#func _ready():
-	#test()
-	#print(test())
-	#var return_value = test()
-	#print(return_value)
-	#var add_result = add(3,3)
-	#print(add_result)
+func _ready():
+	pass
 
 func _process(delta):
 	pass
@@ -24,10 +19,3 @@ func _physics_process(delta):
 		apply_force(Vector2(zero,forceY))
 	if Input.is_action_pressed("move_up"):
 		apply_force(Vector2(zero,-forceY))
-	
-#func test():
-	#var t = 10
-	#return (t+10) 
-#
-#func add(num1, num2):
-	#return num1 + num2
